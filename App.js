17269f67916266fe005fd38be1a07cac1c12ab16
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-01 16:53:12 +0900

    #5.3 Realm SDK

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import styled from "styled-components/native";
+import React, { useState } from "react";
 import Navigator from "./navigator";
+import Realm from "realm";
+import AppLoading from "expo-app-loading";
+
+/* DB의 형태인 Schema 생성 */
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const startLoading = async () => {
+    /* realm DB에 연결 */
+    const realm = await Realm.open({
+      path: "nomadDiaryDB",
+      schema: [FeelingSchema],
+    });
+    console.log(realm);
+  };
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <Navigator />
